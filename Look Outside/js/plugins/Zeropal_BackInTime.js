e300e9a30a5d494190bb4e16944d898a1212fd8c
Turese Anderson <25139527+Turese@users.noreply.github.com>
2026-04-05 22:29:36 -0700

    add bases and planning for 2 new plugins

---
+++ Look Outside/js/plugins/Zeropal_BackInTime.js
@@ -0,0 +1,27 @@
+/**
+ * @target MZ
+ * @name Zeropal_BackInTime
+ * @plugindesc Back in time: speak to the calendar to get the option to travel through time!
+ * @authors 0palite
+ * @version 1.0
+ * @license Unlicensed
+ * @help
+ */
+
+
+
+var BackInTime = BackInTime || {};
+
+BackInTime.applyChanges = function () {
+    //newDay common event trigger
+    // set date and time to 6 on whatever day
+
+    // separate dialogue box: think about your regrets
+    // if failed helen quest: if (1086, set 1086 missedhelenwatering = off, 35 recruitedhelen = on)
+    // if failed ernest quest (if 797, set 797 ernesttoolate = off)
+    // if cave in happened: 
+    // if kicked masked shadow out
+
+};
+
+BackInTime.applyChanges();
---
+++ Look Outside/js/plugins/Zeropal_UndeadFriends.js
@@ -0,0 +1,30 @@
+/**
+ * @target MZ
+ * @name Zeropal_UndeadFriends
+ * @plugindesc Undead Friends: murdering people doesn't stop you from making friends!
+ * @authors 0palite
+ * @version 1.0
+ * @license Unlicensed
+ * @help
+ */
+
+
+
+var Zeropal_UndeadFriends = Zeropal_UndeadFriends || {};
+
+Zeropal_UndeadFriends.applyChanges = function () {
+
+    // update jeanne so her dead event state is the same as her alive event state
+    // update all 4 astronomers for the same
+        // also update all 4 astronomers so they still make it to the roof even if theyre dead
+    // update joel so he can still be recruited if he dies
+    // update harriet in her house so she can still be spoken to when she dies
+    // update harriet door encounter so she can still be spoken to when she dies
+    // update all door encounters so they arent taken from the pool when killed
+    // update mutt so his shop doesnt change at all when hes dead
+    // update eugene so he still restocks and sells when hes dead
+    // update squeakums so he still leads you away if he dies
+
+};
+
+Zeropal_UndeadFriends.applyChanges();
