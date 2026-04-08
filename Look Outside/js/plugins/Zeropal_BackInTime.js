395e4b5843ecf28fde76b2c1d046d3f8fce50e21
Turese Anderson <25139527+Turese@users.noreply.github.com>
2026-04-07 18:38:25 -0700

    add more regrets

--- Look Outside/js/plugins/Zeropal_BackInTime.js
+++ Look Outside/js/plugins/Zeropal_BackInTime.js
@@ -8,41 +8,235 @@
  * @help
  */
 
+var BackInTime = BackInTime || {};
+
 // checks for sophie in the special door encounter pool
 function sophieAtDoor() {
   const special = gVr(166);
   return special.includes(63);
 }
 
-var BackInTime = BackInTime || {};
-
-function fixMuttRegret() {
+function fixMuttKilled() {
   sSw(317, false); // muttDead = OFF
 }
 
-function fixPlantRegret() {
+function fixPlantHealth() {
   sVr(128, 50); // plantHealth = 50
 }
 
-function fixSophieRegret() {
+function fixSophieGone() {
   sSw(362, true); // recruitedSophie = ON
+}
+
+function fixPapKilled() {
+  sSw(362, true); // recruitedSophie = ON
+}
+
+function fixLyleKilled() {
+  sSw(832, false); // killedLyle = OFF
+  delete $gameSelfSwitches._data[[9, 14, 'C'].toString()]; // delete the dead lyle state on his event
+}
+
+function fixEugeneKilled() {
+  sSw(168, false); // killedEugene = OFF
+}
+
+function fixMaskedShadowGift() {
+  sVr(152, 10); // shadowDispo = 10
+  sVr(150, 6); // shadowState = 6
+  sSw(161, false); // shadowItemLeft = OFF
+}
+
+function fixMaskedShadowRecruit() {
+  sSw(27, true); // recruitedShadow = ON
+}
+
+function fixAstronomersKilled() {
+  sSw(170, false); // killedAurelius = OFF
+  sSw(171, false); // killedAster = OFF
+  sSw(172, false); // killedJasper = OFF
+  sSw(173, false); // killedBeryl = OFF
+}
+
+function fixLockedInOfferings() {
+  sSw(206, false); // lastOfferingGood = OFF
+}
+
+function fixErnestTooLate() {
+  sSw(797, false); // ernestTooLate = OFF
+  sSw(790, false) // colSqueakumDead = OFF
+}
+
+function fixErnestKilled() {
+  sSw(789, false) // ErnestDead = OFF
+}
+
+function fixJeanneKilled() {
+  
+}
+
+function fixJoelKilled() {
+  
+}
+
+function fixHellenQuest() {
+  sSw(1086, false) // MissedHellenWatering = OFF
+  sSw(35, true) // recruitedHellen = ON
+  sSw(1087, true) // hellenSpawned = OFF
+}
+
+function fixBasementBlocked() {
+  // if !1063 openedpitdoor
+  sSw(1097, false); // Earthquake = OFF
 }
 
 const regretOptions = {
   sophie: {
     rCondition: sophieAtDoor,
     rText: "(([!s[362],!s[364]]))Sophie.",
-    rFunction: "fixSophieRegret",
-    rText: [],
+    rFunction: "fixSophieGone",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of Sophie. You wish you could have brought her"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "back to her mother. You close your eyes and pray for her",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["safety..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Suddenly, you feel a lot better about her situation. In fact,"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "you wouldn't be surprised if she's already found her way",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "back to this apartment.",
+        ],
+      },
+    ],
+  },
+  hellenQuestFailed: {
+    rName: "((![1086]))Hellen.",
+    rFunction: "fixHellenQuest",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of Hellen. You really let her down. You close"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "your eyes and vow to do better...",
+        ],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Actually, did you forget to water her plant the other day?"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "Perhaps you only imagined you did. In fact, you feel as",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["though Hellen is waiting for you right now."],
+      },
+    ]
   },
   plant: {
     rName: "(([v[128]>5]))Your plant.",
-    rFunction: "fixPlantRegret",
-    rText: [],
+    rFunction: "fixPlantHealth",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of your plant. You haven't been taking care of it"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "very well, and now you fear it may be too late. You close",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["your eyes and hope..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Suddenly, you feel a lot more optimistic. In fact,"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "it's looking greener already!",
+        ],
+      },
+    ],
   },
-  mutt: {
+  muttKilled: {
     rName: "(([!s[317]]))Mutt.",
-    rFunction: "fixMuttRegret",
+    rFunction: "fixMuttKilled",
     rText: [
       {
         code: 101,
@@ -86,9 +280,359 @@ const regretOptions = {
       {
         code: 401,
         indent: 2,
-        parameters: ["now..."],
+        parameters: ["now."],
       },
     ],
+  },
+  shadowGift: {
+    rName: "(([!v[152]<10;v[150]=7]))The Masked Shadow.",
+    rFunction: "fixMaskedShadowGift",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of the masked shadow. If only you had treated it"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "better. You close your eyes and wish it had given you a",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["nicer gift..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Now that you think of it, it did seem to like you a lot."],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "In fact, you'd be willing to bet that it's out searching",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["for a better gift right now."],
+      },
+    ],
+  },
+  shadowRecruit: {
+    rName: "(([!s[27];v[150]>=9]))The Masked Shadow.",
+    rFunction: "fixMaskedShadowRecruit",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of the masked shadow. You wish you hadn't"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "rebuked it so harshly. You close your eyes and wish there",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["was a way to let it know it's still welcome..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Actually, it may already know. In fact, you wouldn't be"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "surprised if it was in your kitchen right now.",
+        ],
+      },
+    ],
+  },
+  lockedInOffering: {
+    rName: "(([!s[27];v[150]>=9]))The Masked Shadow.",
+    rFunction: "fixLockedInOfferings",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of the offerings. You told Jasper you were ready,"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "but now you're having second thoughts. You close your eyes",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["and think..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["There's nothing wrong with going back and asking him again,"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "is there? In fact, you feel as though he may have forgotten",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["to go up to the roof..."],
+      },
+    ],
+  },
+  astronomersKilled: {
+    rName: "(([!s[170],s[171],s[172],s[173]]))The Astronomers.",
+    rFunction: "fixAstronomersKilled",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of the astronomers. You wish you hadn't hurt"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "any of them. They were only trying to help. You close",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["your eyes and vow to do better..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Actually, did you even attack any of them?"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "You don't think you did. In fact, you're quite certain",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["that all four of them are just fine."],
+      },
+    ]
+  },
+  ernestTooLate: {
+    rName: "(([!s[797];!s[790]]))Ernest.",
+    rFunction: "fixErnestTooLate",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of Ernest. You wish you could have been there"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "for him when he needed you most. You close your eyes and",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["vow to do better..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Actually, it may not be too late! In fact, you feel as"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "though you should go check on Ernest today...",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["now."],
+      },
+    ]
+  },
+  ernestKilled: {
+    rName: "(([!s[789]]))Ernest.",
+    rFunction: "fixErnestKilled",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of Ernest. You wish you hadn't done what"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "you did, it was unnecessary. You close your eyes and",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["vow to do better..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Actually, you didn't do a thing to him! In fact, you feel as"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "though you should go check on Ernest today...",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["now."],
+      },
+    ]
+  },
+  eugeneKilled: {
+    rName: "((!s[789]))Eugene.",
+    rFunction: "fixEugeneKilled",
+    rText: [
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["You think of Eugene. You know you murdered him and all"],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "but you wish he didn't have to be so dead about it. You",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["close your eyes and vow to do better..."],
+      },
+      {
+        code: 101,
+        indent: 2,
+        parameters: ["", 0, 0, 2, ""],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["Actually, is he dead? You might have made that part up."],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: [
+          "In fact, you feel as though he's healthier than ever right",
+        ],
+      },
+      {
+        code: 401,
+        indent: 2,
+        parameters: ["now."],
+      },
+    ]
+  },
+  basementBlocked: {
+    rName: "(([s[1097];!s[1063]]))The basement pit.",
+    rFunction: "fixBasementBlocked",
+    rText: []
   },
 };
 
@@ -145,7 +689,7 @@ BackInTime.applyChanges = function () {
       },
       {
         code: 0,
-        indent: indent + 1,
+        indent: 2,
         parameters: [],
       },
     ];
@@ -184,11 +728,6 @@ BackInTime.applyChanges = function () {
       const week2OptionIntro = createOptionIntro(week2OptionNames, 2);
 
       const dateOptions = [
-        {
-          code: 401,
-          indent: 1,
-          parameters: ["Set current day?"],
-        },
         {
           code: 102,
           indent: 1,
@@ -261,14 +800,14 @@ BackInTime.applyChanges = function () {
       ];
 
       const createRegretOptions = function () {
-        const regretsList = ["sophie", "mutt", "plant"];
+        const regretsList = Object.keys(regretOptions);
 
         const regretNames = [];
         const regretCases = [];
 
         let index = 1;
 
-        regretsList.forEac-h((regretName) => {
+        regretsList.forEach((regretName) => {
           const options = regretOptions[regretName];
           if (!options.rCondition || options.rCondition()) {
             regretNames.push(options.rName);
